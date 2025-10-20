c08cc5a8be11496b05ef85dbad65977c0d444559
Adler-FSA <info@finanziellesouveraenitaet.de>
2025-10-20 14:58:51 +0200

    add: block-04-intro.js (Platzhalter Begrüßungsblock DE/EN für grundkurs-4.html)

---
+++ library/js/text/block-04-intro.js
@@ -0,0 +1,14 @@
+// Baustein 04 – Begrüßungsblock (DE/EN) für grundkurs-4.html
+
+const block04_intro = {
+  de: {
+    line1: "[Platzhalter – Begrüßung Grundkurs 4 (DE)]",
+    line2: "[Platzhalter – Beschreibung Grundkurs 4 (DE)]",
+    line3: "[Platzhalter – Abschluss-Hinweis Grundkurs 4 (DE)]"
+  },
+  en: {
+    line1: "[Placeholder – Introduction Basic Course 4 (EN)]",
+    line2: "[Placeholder – Description Basic Course 4 (EN)]",
+    line3: "[Placeholder – Closing Note Basic Course 4 (EN)]"
+  }
+};
