8d2fb2a721db80ddd0ccbd40008fce87bb512874
Adler-FSA <info@finanziellesouveraenitaet.de>
2025-10-20 14:59:41 +0200

    add: block-05-intro.js (Platzhalter Begrüßungsblock DE/EN für prüfung.html)

---
+++ library/js/text/block-05-intro.js
@@ -0,0 +1,14 @@
+// Baustein 05 – Begrüßungsblock (DE/EN) für prüfung.html
+
+const block05_intro = {
+  de: {
+    line1: "[Platzhalter – Begrüßung Prüfungsseite (DE)]",
+    line2: "[Platzhalter – Erklärung Prüfungsablauf (DE)]",
+    line3: "[Platzhalter – Abschluss-Hinweis Zertifikat (DE)]"
+  },
+  en: {
+    line1: "[Placeholder – Introduction Exam Page (EN)]",
+    line2: "[Placeholder – Explanation Exam Process (EN)]",
+    line3: "[Placeholder – Closing Note Certificate (EN)]"
+  }
+};
