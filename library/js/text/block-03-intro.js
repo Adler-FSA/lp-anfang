e8ea3d8d85b70e2aefb74dffe0abfce80b11fb54
Adler-FSA <info@finanziellesouveraenitaet.de>
2025-10-20 14:57:44 +0200

    add: block-03-intro.js (Platzhalter Begrüßungsblock DE/EN für grundkurs-3.html)

---
+++ library/js/text/block-03-intro.js
@@ -0,0 +1,14 @@
+// Baustein 03 – Begrüßungsblock (DE/EN) für grundkurs-3.html
+
+const block03_intro = {
+  de: {
+    line1: "[Platzhalter – Begrüßung Grundkurs 3 (DE)]",
+    line2: "[Platzhalter – Beschreibung Grundkurs 3 (DE)]",
+    line3: "[Platzhalter – Abschluss-Hinweis Grundkurs 3 (DE)]"
+  },
+  en: {
+    line1: "[Placeholder – Introduction Basic Course 3 (EN)]",
+    line2: "[Placeholder – Description Basic Course 3 (EN)]",
+    line3: "[Placeholder – Closing Note Basic Course 3 (EN)]"
+  }
+};
